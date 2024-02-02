440f4eeab1150c361dc08ad1ec63604f8bec3094
KotyshevaYulya <92879736+KotyshevaYulya@users.noreply.github.com>
2024-02-02 19:36:40 +0100

    done hw

---
+++ js/task-2.js
@@ -0,0 +1,45 @@
+'use strict'
+function calcAverageCalories(days) {
+    let totalCalories = 0;
+    for (const day of days) {
+        totalCalories += day.calories;
+    }
+    // const averageDailyValue = totalCalories / days.length;
+    // console.log(averageDailyValue)
+
+    let averageDailyValue = 0;
+
+    if (totalCalories > 0) {
+       return averageDailyValue = totalCalories / days.length;
+    } else {
+        return averageDailyValue;
+    }
+}
+
+console.log(
+  calcAverageCalories([
+    { day: "monday", calories: 3010 },
+    { day: "tuesday", calories: 3200 },
+    { day: "wednesday", calories: 3120 },
+    { day: "thursday", calories: 2900 },
+    { day: "friday", calories: 3450 },
+    { day: "saturday", calories: 3280 },
+    { day: "sunday", calories: 3300 }
+  ])
+); // 3180
+
+console.log(
+  calcAverageCalories([
+    { day: "monday", calories: 2040 },
+    { day: "tuesday", calories: 2270 },
+    { day: "wednesday", calories: 2420 },
+    { day: "thursday", calories: 1900 },
+    { day: "friday", calories: 2370 },
+    { day: "saturday", calories: 2280 },
+    { day: "sunday", calories: 2610 }
+  ])
+); // 2270
+
+console.log(
+  calcAverageCalories([])
+); // 0
